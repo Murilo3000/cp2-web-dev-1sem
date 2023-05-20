71a3da1908c37541251fe5dc432126ca1a63cb31
Pedro Henrique <pedro.phoa1405@gmail.com>
2023-05-20 17:39:40 -0300

    js pronto-pedro

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
 function welcome(){
     alert("Bem vindo ao Kit On")
     window.open("../index.html")
- }
-let data = new Date()
-
-let dia = String(data.getDate()).padStart(2,0)
-
-let mes = String(data.getMonth()).padStart(2,0)
-
-let ano = data.getFullYear()
-
-let dataAtual = `${dia}/${mes}/${ano}`
-
+}
 function validateFields(){
     const emailValid = isEmailValid;
     const passwordValid = isPasswordValid;
     document.getElementById("button").disabled = !emailValid || !passwordValid;
- }
+}
  function isEmailValid(){
      const email=document.getElementById("emailForm").value;
      if (!email){
          return false;
      } return validateEmail(email);
- }
+}
  function isPasswordValid(){
      const password = getElementById("passwordForm").value;
      if (!password){
          return false;
      } return true;
- }
+}
  function validateEmail(email) {
      return /\S+@\S+\.\S+/.test(email);
- }
+}
  function trocar(cor){
     document.body.style.background = cor
 }
